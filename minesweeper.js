4e878949423336ae8c65846a8c34481904d08807
Jay Westfold <39254665+Aurianchiamh@users.noreply.github.com>
2018-05-17 14:34:06 +1000

    Add files via upload

---
+++ minesweeper.js
@@ -0,0 +1,16 @@
+//Hard code of what a board will look like
+const blankLine = '  |   |  ';
+
+console.log('this is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+//Hard code of what a board will look like with guesses and mines
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+console.log('this is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
